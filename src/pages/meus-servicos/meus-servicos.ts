082accc3c91a54cc28453c15b8354a0873037d2c
unknown <jeanoliveiragomes1@hotmail.com>
2018-08-20 15:58:14 -0300

    Funcionalidade apagar meu serviço/anuncio

--- src/pages/meus-servicos/meus-servicos.ts
+++ src/pages/meus-servicos/meus-servicos.ts
@@ -15,7 +15,8 @@ import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angu
   templateUrl: 'meus-servicos.html',
 })
 export class MeusServicosPage {
-
+  public refresher;
+  public isRefresher: boolean = false;
   public meus_anuncios = new Array<any>();
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public servicoProvider: ServicoProvider, public toastCtrl: ToastController) {
@@ -26,9 +27,31 @@ export class MeusServicosPage {
     console.log('ionViewDidLoad MeusServicosPage');
   }
 
+  deletar(anuncioId: string){
+    this.servicoProvider.deletar(anuncioId).subscribe((data: any)=>{
+      let toastg = this.toastCtrl.create({
+        message: 'Anuncio Apagado Com Sucesso',
+        duration: 3000,
+        position: 'top'
+        });
+      toastg.present();
+      this.getMeusServicos();
+  },  error =>{
+    let toastg = this.toastCtrl.create({
+      message: 'Falha ao buscar serviços, Tente Novamente',
+      duration: 3000,
+      position: 'top'
+      });
+    toastg.present();
+  });
+  }
+
   getMeusServicos(){
     this.servicoProvider.getMeusServicos().subscribe((data: any)=>{
         this.meus_anuncios= data;
+        if (this.refresher) {
+          this.refresher.complete();
+          this.isRefresher = false;}
     },  error =>{
       let toastg = this.toastCtrl.create({
         message: 'Falha ao buscar serviços, Tente Novamente',
@@ -36,7 +59,14 @@ export class MeusServicosPage {
         position: 'top'
         });
       toastg.present();
+      
     });
+  }
+
+  doRefresh(refresher) {
+    this.refresher = refresher;
+    this.isRefresher = true;
+    this.getMeusServicos();
   }
 
 }
